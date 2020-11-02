6a92b2c0e194f0d3a8ebdae224877c3cb346dae3
Alexander Radyushin <r.alex.andr@ya.ru>
2020-11-02 16:26:16 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export default function someFn(): number {
-  return 200;
-}
+import { Promise } from 'bluebird';
+import {
+  createClient as createRedisClient,
+  RedisClient as Client,
+  ClientOpts as Options,
+} from 'redis';
+
+//
+const { REDIS_HOST, REDIS_PORT } = process.env;
+
+type Units = 'PX' | 'EX';
+type Period = number;
+type Value = string | number;
+
+export type RedisClient = Client & {
+  setAsync(key: string, value: Value, units?: Units, period?: Period): Promise<void>;
+  getAsync(key: string): Promise<string>;
+  options: Options;
+};
+
+// Create client to work with Redis database
+Promise.promisifyAll(Client.prototype);
+export const redis = createRedisClient({
+  host: `${REDIS_HOST}`,
+  port: parseInt(`${REDIS_PORT}`, 10),
+}) as RedisClient;
+
+export default redis;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,27 @@
-import * as assert from 'assert';
-import someFn from '../src';
+import { redis } from '../src';
 
+const CACHE_KEY = 'test_cache_key';
+const CACHED_VALUE = 'test_cache_value';
 
-describe('Test someFn', function () {
-  it('No error expected', async function () {
-    const result = someFn();
+describe('Test caching database', function () {
+  beforeAll(async () => {
+    console.log('Emit beforeAll test-hook');
+  });
 
-    expect(result).toBe(200);
+  afterAll(async () => {
+    console.log('Emit afterAll test-hook');
+    redis.end();
+  });
+
+  it('Save value to cache', async function () {
+    const result = await redis.setAsync(CACHE_KEY, CACHED_VALUE);
+
+    expect(result).toBe('OK');
+  });
+
+  it('Get value from cache', async function () {
+    const result = await redis.getAsync(CACHE_KEY);
+
+    expect(result).toBe(CACHED_VALUE);
   });
 });
